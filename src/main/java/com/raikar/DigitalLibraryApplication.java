aade723c5430cc8c0c91bb8c7dcf93f654a8d482
Meghana Shet <megharaikar6@gmail.com>
2024-08-01 18:53:50 +0530

    Update DigitalLibraryApplication.java

--- src/main/java/com/raikar/DigitalLibraryApplication.java
+++ src/main/java/com/raikar/DigitalLibraryApplication.java
@@ -8,7 +8,7 @@ public class DigitalLibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalLibraryApplication.class, args);
-		System.out.println("Application Started");
+		System.out.println("Application");
 	}
 
 }
